fd50f1d6af27291bad1220ca8bf28aaef5e08506
Stephen Stranahan <49105822+sstranahan@users.noreply.github.com>
2021-10-28 11:32:41 -0400

    Add files via upload
    Adding utility files: Matlab script to calc OCC vals, OCC vals table, piano chart, MICRO_DAC schematic

---
+++ UTILS/calcNoteFreqs.m
@@ -0,0 +1,18 @@
+% Script to calculate timer output compare values for western musical scale
+% C-major scale notes only
+% 10/16/2021 - Stephen Stranahan
+
+freqScale = [65.41  73.42  82.41  87.31  98.00  110.00 123.47   % Octave C2 - B2
+             130.81 146.83 164.81 174.61 196.00 220.00 246.94   % Octave C3 - B3
+             261.63 293.67 329.63 349.23 392.00 440.00 493.88   % Octave C4 - B4
+             523.25 587.33 659.26 698.46 783.99 880.00 987.77   % Octave C5 - B5
+             1046.5 1174.7 1318.5 1396.9 1568.0 1760.0 1975.5]; % Octave C6 - B6
+         
+clkFreq = 16000000; 
+
+freqPeriod = 1./freqScale;
+
+clkPeriod = 1/clkFreq;
+
+numTicks = ((freqPeriod./clkPeriod)./2)-1;  % This array holds the correct output compare
+                                            % values for 16MHz clock MCU
